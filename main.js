f263c8d12cd0554b5f141b7974fa2876acebb8a9
Pradyumna Kaushal <kaushal.pradyumna@gmail.com>
2024-06-18 15:18:30 +0530

    jump

--- main.js
+++ main.js
@@ -1,5 +1,56 @@
 const health=100
+const playerSpeed=50
+const playerJump=1
+let centreX=400
+let centreY=300
 
+const canvas = document.getElementById("gameboardCanvas");
+const ctx = canvas.getContext("2d");
+
+
+class Player{
+    constructor(x,y,size,color){
+        this.x=x
+        this.y=y
+        this.size=size
+        this.color=color
+        this.jumpHeight=10
+        this.shouldJump=false
+        this.jumpCounter=0
+
+    }
+
+    jump(){
+        if(this.shouldJump){
+            this.jumpCounter++
+            if(this.jumpCounter<15){
+                this.y-=this.jumpHeight
+            }
+            else if(this.jumpCounter>14 && this.jumpCounter<19){
+                this.y+=0
+            }
+            else if(this.jumpCounter<33){
+                this.y+=this.jumpHeight
+            }
+
+            if (this.jumpCounter>=32){
+                this.shouldJump=false
+            }
+
+        }
+    }
+
+    draw(){
+        this.jump()
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x,this.y,this.size/2,this.size);
+
+
+    
+    }
+
+
+}
 
 
 
@@ -24,35 +75,74 @@ function startGame(){
     
     //pause and play features
 
+    const player=new Player(centreX,centreY,150,'red')
 
-    function draw(x,y) {
-        const canvas = document.getElementById("canvas");
-        if (canvas.getContext) {
-          const ctx = canvas.getContext("2d");
+    function animate(){
+        requestAnimationFrame(animate)  
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-          ctx.fillStyle = "rgb(255, 0, 0)";
-          ctx.fillRect(x, y, 100, 150);
+        player.draw()
 
-          
-        }
-      }
-
-    const centreX=400
-    const centreY=300
-
-
-    draw(400,300);
-
-    window.addEventListener("keydown", checkKeyPressed, false);
-
-    function checkKeyPressed(evt) {
-        if (evt.keyCode == "13") {
-            alert("You pressed 'enter'.");
-        }
+        
     }
-      
+    animate()
+
+
+    
+    addEventListener("keydown", e => {
+        if(e.code === 'Space'){
+            if(!player.shouldJump){
+                player.jumpCounter = 0;
+                player.shouldJump = true;
+
+            }
+        }
+    });
 
 
 
-      
+        
+
+        // if (evt.keyCode == "68") {
+        //     centreX+=playerSpeed
+        //     // alert(centreX)
+        //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //     draw();
+
+        
+        // }
+
+        // if (evt.keyCode=='65'){
+        //     centreX-=playerSpeed 
+        //     ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //     draw();
+
+        // }
+
+ 
+
+
+
+            
+        
+        
+        
+
+    
 }
+
+
+
+
+    
+
+    
+      
+
+
+
+      
+
+
+
+
